ba38b0c3efdd61b13ca960ae106d5b19b33bfd32
Vaibhav Garg <67408018+vaibhavgarg237@users.noreply.github.com>
2022-02-20 14:17:36 +0530

    chall 4 & 5

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var mapping = ['', 'images/dice1.png', 'images/dice2.png', 'images/dice3.png', 'images/dice4.png', 'images/dice5.png', 'images/dice6.png'];
+
+document.getElementsByClassName("img1")[0].setAttribute("src", mapping[randomNumber1]);
+document.getElementsByClassName("img2")[0].setAttribute("src", mapping[randomNumber2]);

6060d033d9c676d9b6c683132b83f3cc5081ee5f
Vaibhav Garg <67408018+vaibhavgarg237@users.noreply.github.com>
2022-02-20 16:13:15 +0530

    nits

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
+// CAN ALSO USE CONCATENATION 
+//generate random nos. and add in b/w of"dice" & ".png"
 var mapping = ['', 'images/dice1.png', 'images/dice2.png', 'images/dice3.png', 'images/dice4.png', 'images/dice5.png', 'images/dice6.png'];
 
 document.getElementsByClassName("img1")[0].setAttribute("src", mapping[randomNumber1]);
 document.getElementsByClassName("img2")[0].setAttribute("src", mapping[randomNumber2]);
 
+//can also use querySelectorAll 
 var res1 = document.getElementsByClassName("img1")[0].getAttribute("src").slice(11, 13);
 var res2 = document.getElementsByClassName("img2")[0].getAttribute("src").slice(11, 13);
 if (res1 == res2) {
     //draw
     document.getElementsByTagName("h1")[0].innerText = "Draw";
+    //document.querySelector("h1").innerText = "Draw";
 } else if (res1 > res2) {
     //player 1 wins
-    document.getElementsByTagName("h1")[0].innerText = "Player1 wins";
+    document.getElementsByTagName("h1")[0].innerText = "🚩 Player1 wins";
 } else {
     //player 2 wins
-    document.getElementsByTagName("h1")[0].innerText = "Player2 wins";
+    document.getElementsByTagName("h1")[0].innerText = "Player2 wins 🚩";
 }
